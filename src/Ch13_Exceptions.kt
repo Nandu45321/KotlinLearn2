50d3c16050d3705b2e5772c4fc14a310dd2da11c
nandu <69576886+Nandu45321@users.noreply.github.com>
2025-05-24 00:02:53 +0530

    Loops

--- src/Ch13_Exceptions.kt
+++ src/Ch13_Exceptions.kt
@@ -1,5 +1,22 @@
 fun main() {
 
+    try {
+        val result = 10 / 0
+    } catch (e: Exception){
+        println("Exception is ${e.message}")
+    }
+
+    try {
+        val data = "123a"
+        val number = data.toInt()
+        println(data)
+    } catch (e: NumberFormatException) {
+        println("Excptin: ${e}")
+    } catch (e: Exception) {
+        println(e.message)
+    } finally {
+        println("This message will always be executed")
+    }
 
 
 }
