cde36b625e2b7f81580b1dea371fee2000a15784
nandu <69576886+Nandu45321@users.noreply.github.com>
2025-05-25 00:50:21 +0530

    TypeChecking

---
+++ src/Ch15_TypeCheckAndSmartCasts.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    println(typeChecker("Hi"))
+    println(typeChecker(49))
+    println(typeChecker(48.9))
+
+}
+
+fun typeChecker(obj: Any): String {
+    when(obj){
+        is String -> return "String of length ${obj.length}"
+        is Int -> return "Integer"
+    }
+    return "We do not know"
+}
