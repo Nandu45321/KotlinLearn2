138b343d70d8992e2868571a398df552ae8562b4
nandu <69576886+Nandu45321@users.noreply.github.com>
2025-05-26 02:06:34 +0530

    Getter and Setters

---
+++ src/Ch22_DataClasses.kt
@@ -0,0 +1,25 @@
+
+data class User(val name: String, val age: Int) {
+
+    override fun toString(): String {
+        return "Name: $name, Age: $age"   // uses this when println(user1/2) is called
+    }
+}
+
+fun main() {
+
+    val user1 = User("Allice", 30)
+    val user2 = User("Allice", 30)
+
+    println(user1 == user2)  // true
+    println(user1)
+
+    val user3 = user1.copy()  //Will copy. These are the same data wise, but the memory allocated is different
+
+    println("Is data the same? : ${ user1 == user3 }")
+    println("Is reference the same? : ${ user1 === user3 }") // Checks for reference (is)
+
+    println("1st component = ${user3.component1()}")
+    println("2nd component = ${user3.component2()}")
+
+}
