b54a24ea83f6eaca000f49fbd2cfcb8e3b59d445
nandu <69576886+Nandu45321@users.noreply.github.com>
2025-05-28 01:24:44 +0530

    object

---
+++ src/Ch29_Delegation.kt
@@ -0,0 +1,40 @@
+interface Sound {
+    fun makeSound()
+}
+
+class Dog: Sound {
+    override fun makeSound() {
+        println("Bark")
+    }
+}
+
+class Cat: Sound {
+    override fun makeSound() {
+        println("Meow")
+    }
+}
+
+//Past example, kinda
+class Example {
+    val lazyValue: String by lazy {
+        println("computed")         //lazy block gets init and not the var
+        "hello"
+    }
+}
+
+//So whatever we get as the argument, we'll be implementing that override function
+class PetShop(private val sound: Sound) : Sound by sound
+
+fun main() {
+
+    val dog = Dog()
+    val cat = Cat()
+
+    val petShopDog = PetShop(dog)
+    val petShopCat = PetShop(cat)
+
+    petShopDog.makeSound()
+    petShopCat.makeSound()
+
+
+}
